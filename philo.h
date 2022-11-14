c27b64e7eb0afab58c2bc36bff959ffc22af9464
markov199 <99345543+markov199@users.noreply.github.com>
2022-11-14 15:04:22 +0400

    Create philo.h

---
+++ philo.h
@@ -0,0 +1,56 @@
+
+#ifndef PHILO_H
+#define PHILO_H
+
+#include<pthread.h>
+#include<stdio.h>
+#include<stdbool.h>
+#include<sys/time.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include"message.h"
+
+typedef unsigned int    t_ms;
+
+typedef char            *t_error;
+
+typedef struct s_fork{
+    pthread_mutex_t fork_mutex;
+    bool used;
+}   t_fork;
+
+typedef struct s_queue{
+    pthread_mutex_t queue_mutex;
+    int *priority_queue;
+    int queue_count;
+} t_queue;
+
+
+typedef struct s_table{
+    int num_philo;
+    int max_meals;
+    t_ms start_time;
+    t_ms time_to_die;
+    t_ms time_to_eat;
+    t_ms time_to_sleep;
+    bool philo_dead;
+    t_fork *forks;
+    t_queue *philo_queue;
+    struct s_philo *philo;
+    pthread_mutex_t mutex_print;
+    pthread_mutex_t mutex_thread;
+}   t_table;
+
+typedef struct s_philo{
+    int id;
+    int meals_eaten;
+    bool has_fork;
+    int state; /*thinking = 1, taken fork = 2, eating = 3, sleeping = 4, died = 5 */
+    t_ms last_meal;
+    t_table *table_data;
+    pthread_t threads;
+    t_ms time_start;
+    t_ms time_last_action;
+}   t_philo;
+
+# endif
